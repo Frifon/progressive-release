44417137a65ba5108d3dad7752dd50d92f99f27b
Sergey Bugaev <bugaevc@gmail.com>
2015-07-31 13:07:03 +0300

    Don't clear old posts on start

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ __author__ = 'dimagalov'
 from aggregator.aggregator import get_users, aggregator
 from database.query import delete_all
 
-delete_all()
+#delete_all()
 
 aggregator(get_users('users_club_22079806'), best_post_delay = 10, max_old_post = 24 * 60 * 60, log = 1)
