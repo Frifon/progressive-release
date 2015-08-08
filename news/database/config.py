d2b3bb53651ed69a9e86ee2ad76c4ba062f35c6e
Roman Lozko <lozko@179.ru>
2015-08-08 12:06:11 +0300

    MERGE dev with release. Also database temporarily changed to local, use create_db again

--- news/database/config.py
+++ news/database/config.py
@@ -1,3 +1,3 @@
 from sqlalchemy import create_engine
 import os.path
-engine = create_engine('mysql+pymysql://emil:very_secret_pass@149.5.2.221/vk', encoding='utf-8')
+engine = create_engine('sqlite:///' + os.path.abspath(__file__) + '/../vk.db')
