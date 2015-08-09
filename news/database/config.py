5c7084c166074c665a65d6277056b043ab80c097
MaverickChaser <theluckyemil@gmail.com>
2015-08-09 16:41:14 +0300

    Update config.py

--- news/database/config.py
+++ news/database/config.py
@@ -1,7 +1,3 @@
 from sqlalchemy import create_engine
 import os.path
-<<<<<<< HEAD
 engine = create_engine( 'sqlite:///' + os.path.abspath(__file__) + '/../vk.db')
-=======
-engine = create_engine('sqlite:///' + os.path.abspath(__file__) + '/../vk.db')
->>>>>>> dev
