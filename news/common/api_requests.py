680e874d6e202d82172b2b7feca31c77847e77a1
Roman Lozko <lozko@179.ru>
2015-08-11 12:17:12 +0300

    Seems working

--- news/common/api_requests.py
+++ news/common/api_requests.py
@@ -17,12 +17,54 @@ class Add_request:
     callbacks = deque()
     _execute_mutex = False  # Preventing stack overflow
 
-    def execute_requests(this):
+    # add_photo_request() variables
+    photo_limit = 100    # how much photos to get in 1 request
+    photo_values_in_process = deque()
+    photo_values = deque()
+    photo_callbacks = deque()
+
+
+    def photos_callback(responce):
+        self = Add_request 
+        popped_callbacks = 0
+        if self._debug:
+            print('photos_callback')
+
+        for photo_info in responce:
+            processed = False
+
+            while len(self.photo_values_in_process) > 0 and not processed:
+                photo_id = self.photo_values_in_process.popleft().split('_')[1]
+                if photo_id != str(photo_info['id']):
+                    if self._debug:
+                        print('BAD', photo_id, '!=', photo_info['id'])
+                    self.photo_callbacks.popleft()(False)
+                else:
+                    if self._debug:
+                        print("GOOD")
+                    self.photo_callbacks.popleft()(photo_info)
+                    processed = True
+                popped_callbacks += 1
+
+                if self._debug and len(self.photo_values_in_process) > 0:
+                    print(self.photo_values_in_process[0], end=' ')
+
+            if len(self.photo_values_in_process) == 0 and not processed:
+                print('FATAL ERROR IN news.common.api_requests.Add_request.photos_callback')
+                print('REQUEST FOR PHOTO IS MISSING')
+                exit(-1)
+
+        while popped_callbacks < self.photo_limit:
+            self.photo_callbacks.popleft()(False)
+            popped_callbacks += 1
+
+    def execute_requests(self):
         Add_request._execute_mutex = True
 
         while True:
             try:
-                # print('try', end=' ')       # DEBUG PRINT
+                if self._debug:
+                    print('try', end=' ')       # DEBUG PRINT
                 vk_api = vk_api_authorization()
                 break
                 '''
@@ -35,19 +77,20 @@ class Add_request:
                 exit(0)
             except:
                 pass
-        # print('\nsuccess!')                 # DEBUG PRINT
+        if self._debug:
+            print('\nsuccess!')                 # DEBUG PRINT
 
-        while len(this.requests) > 0:
-            if this._debug:
-                # print('execute started...', end=' ') # DEBUG PRINT
+        while len(self.requests) >= self.execute_limit:
+            if self._debug:
+                print('execute started...', end=' ') # DEBUG PRINT
                 pass
 
             current_requests = 0
             request = 'return ['
 
-            while len(this.requests) > 0 and current_requests < this.execute_limit:
-                now = this.requests.popleft()
-                if this._debug:
+            while len(self.requests) > 0 and current_requests < self.execute_limit:
+                now = self.requests.popleft()
+                if self._debug:
                     print('Added request ', now[0], ', values ', now[1])    # DEBUG PRINT
                 request += 'API.{req[0]}({req[1]}), '.format(req=now)
                 current_requests += 1
@@ -63,29 +106,60 @@ class Add_request:
                 except ApiError:
                     print('Too many requests per second. Trying again.')
                 except:
-                    # traceback.print_exc()
+                    if self._debug:
+                        traceback.print_exc()
                     pass
 
-            if this._debug:
-                # print('connected')          # DEBUG PRINT
+            if self._debug:
+                print('connected')            # DEBUG PRINT
                 pass
 
             index = 0
             while current_requests > 0:
-                if this._debug:
+                if self._debug:
                     print('.', end=' ')       # DEBUG PRINT
-                now = this.callbacks.popleft()(resp[index])
+                now = self.callbacks.popleft()(resp[index])
                 index += 1
                 current_requests -= 1
 
-            if this._debug:
+            if self._debug:
                 print('\n---------------')    # DEBUG PRINT
 
         Add_request._execute_mutex = False
 
-    def __init__(this, method, values, callback):
-        Add_request.requests.append([method, str(values)])
-        Add_request.callbacks.append(callback)
-        if Add_request._execute_mutex is False and \
-           len(Add_request.requests) >= Add_request.execute_limit:
-            Add_request.execute_requests(Add_request)
+    def add_photo_request(self, values, callback):
+        if self._debug:
+            print('add_photo_request', values['photos'])
+        self.photo_values.append(values['photos'])
+        self.photo_callbacks.append(callback)
+        if len(self.photo_values) >= self.photo_limit:
+            final_request = []
+            for i in range(self.photo_limit):
+                req = self.photo_values.popleft()
+                final_request.append(req + ',')
+                self.photo_values_in_process.append(req)
+
+            final_request = ''.join(final_request)
+            final_values = {
+                'photos': final_request,
+                'extended': 1
+            }
+            self.requests.append(['photos.getById', str(final_values)])
+            self.callbacks.append(self.photos_callback)
+
+            if self._execute_mutex is False and \
+               len(self.requests) >= self.execute_limit:
+                self.execute_requests(self)
+
+    def __init__(self, method, values, callback):
+        self = Add_request
+
+        if (method == 'photos.getById'):
+            self.add_photo_request(self, values, callback)
+            return
+
+        self.requests.append([method, str(values)])
+        self.callbacks.append(callback)
+        if self._execute_mutex is False and \
+           len(self.requests) >= self.execute_limit:
+            self.execute_requests(self)
